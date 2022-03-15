7075fecb448659044122ea96fe00304652b9d7a9
jeppe-t <jeppethylstrup@yahoo.dk>
2022-03-15 11:39:01 +0100

    Create movie object

---
+++ backend/src/main/java/com/dia/backend/data/mapper/DataMapper.java
@@ -0,0 +1,27 @@
+package com.dia.backend.data.mapper;
+
+import com.dia.backend.data.repository.MovieRepository;
+import com.dia.backend.domain.model.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataMapper implements CommandLineRunner {
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        //Testdata
+        Movie movie = new Movie();
+        movie.setTitle("Test title");
+        movie.setYear(2020);
+        movie.setCountry("Test country");
+        movie.setLanguage("Test Language");
+        movieRepository.save(movie);
+
+    }
+}
