ce9ebbb26f64135cddba20b351466fb560008029
jeppe-t <jeppethylstrup@yahoo.dk>
2022-03-15 21:08:46 +0100

    Post, put, delete method added

--- backend/src/main/java/com/dia/backend/view/controller/MovieController.java
+++ backend/src/main/java/com/dia/backend/view/controller/MovieController.java
@@ -3,9 +3,9 @@ package com.dia.backend.view.controller;
 import com.dia.backend.data.repository.MovieRepository;
 import com.dia.backend.domain.model.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,4 +32,33 @@ public class MovieController {
     }
 
   }
+
+  @PostMapping("/movies/create")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void postMovie(@RequestBody Movie movie) {
+    movieRepository.save(movie);
+  }
+
+  @PutMapping("/movies/{id}")
+  public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
+    Optional<Movie> movie1 = movieRepository.findById(id);
+    if (movie1.isPresent()) {
+      movieRepository.save(movie);
+      return new ResponseEntity<>(movie, HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @DeleteMapping("/movies/{id}")
+  public ResponseEntity<String> deleteMovie(@PathVariable Long id) {
+    try {
+      movieRepository.deleteById(id);
+      return new ResponseEntity<>("Slettet id=" + id, HttpStatus.OK);
+    } catch (Exception err){
+      return new ResponseEntity<>("Jeg kunnne ikke slet id=" + id, HttpStatus.NOT_FOUND);
+    }
+
+  }
+
 }
