c37e1953c3f88edcc05e0c39c1755c089945b789
jeppe-t <jeppethylstrup@yahoo.dk>
2022-03-18 10:12:56 +0100

    Kinohall bugfix

--- backend/src/main/java/com/dia/backend/domain/model/KinoHall.java
+++ backend/src/main/java/com/dia/backend/domain/model/KinoHall.java
@@ -1,20 +1,22 @@
 package com.dia.backend.domain.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class KinoHall {
-  @Id private int kinoHallId;
-  private int row;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name="kinoHallId")
+  private int kinoHallId;
+  private int rowX; //Can be named rowm because its a reserved keyword
   private int seat;
 
-  public int getRow() {
-    return row;
+  public int getRowX() {
+    return rowX;
   }
 
-  public void setRow(int row) {
-    this.row = row;
+  public void setRowX(int row) {
+    this.rowX = row;
   }
 
   public int getSeat() {
