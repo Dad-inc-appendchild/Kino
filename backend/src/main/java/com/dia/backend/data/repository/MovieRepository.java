0ae479f02d08d9c29a0061ec5e29d00b941fee23
MathiasReker <MathiasReker@users.noreply.github.com>
2022-03-22 18:36:59 +0000

    Google Java Format

--- backend/src/main/java/com/dia/backend/data/repository/MovieRepository.java
+++ backend/src/main/java/com/dia/backend/data/repository/MovieRepository.java
@@ -3,5 +3,4 @@ package com.dia.backend.data.repository;
 import com.dia.backend.domain.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface MovieRepository extends JpaRepository<Movie, Integer> {}
