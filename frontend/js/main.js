aa24a9fda765508d29a6911abfa398bbb8a96ed8
jeppe-t <jeppethylstrup@yahoo.dk>
2022-03-24 12:22:31 +0100

    Building frontpage section setup

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -40,50 +40,60 @@ function buildMovie(movie) {
   let image = document.createElement("img");
   image.src = imagesrc;
   image.classList.add("card-img-top");
-  image.classList.add("card");
   image.alt = "img/FrenchFriday.png";
   movieElement.append(image);
 
   let cardBody = document.createElement("div");
   cardBody.classList.add("card-body");
-  cardBody.classList.add("movie-body");
+  //cardBody.classList.add("movie-body");
   let cardHeader = document.createElement("h5");
   cardHeader.innerText = movie.title; //title goes here;
   cardHeader.classList.add("movie-title");
   cardBody.append(cardHeader);
 
-  let cardtext = document.createElement("p");
-  cardtext.innerHTML = "Udgivelsesår: " + movie.year + "<br>" + "Spilletid: " + Math.round(movie.duration) + " Minutter"; //INSERT MORE DATA
-  cardtext.classList.add("card-text");
-  cardtext.classList.add("movie-text");
-  cardBody.append(cardtext);
+  let cardParagraph = document.createElement("p");
+  cardParagraph.innerHTML = "INSERT MOVIE ATTRIBUT DESCRIPTION HERE";
+  cardParagraph.classList.add("card-text");
+  cardBody.append(cardParagraph);
+
+  const cardButton = document.createElement("a");
+  cardButton.innerHTML = "Læs mere";
+  cardButton.href = "#";
+  cardButton.classList.add("btn");
+  cardButton.classList.add("btn-outline-primary");
+  cardBody.append(cardButton);
+
+  const cardButtonLink = document.createElement("a");
+  cardButtonLink.innerHTML = "Se trailer";
+  cardButtonLink.href = "#";
+  cardButtonLink.classList.add("btn");
+  cardButtonLink.classList.add("btn-outline-secondary");
+  cardBody.append(cardButtonLink);
+
   movieElement.append(cardBody);
 
   let cardList = document.createElement("ul"); //screenings?
   cardList.classList.add("list-group");
   cardList.classList.add("list-group-flush");
 
-  for (let i = 0; i < 3; i++) { //change to for each screening?
+  /*for (let i = 0; i < 3; i++) { //change to for each screening?
     let cardElement = document.createElement("li");
     cardElement.classList.add("list-group-item");
     cardElement.innerText = "Screening " + i;
     cardList.append(cardElement);
   }
 
-  let trailerLink = movie.trailerLink;
-  movieElement.append(cardList);
-  cardBody = document.createElement("div");
-  let link = document.createElement("a");
-  link.href = trailerLink;
+   */
 
-  link.classList.add("movie-link")
-  link.innerText = "Trailer link"; //link to something??
-  cardBody.append(link);
+
 
   movieElement.append(cardBody);
 
   movieContainer.append(movieElement);
   return movieContainer;
+
+
+
 }
 
 function makeMovieList() {
